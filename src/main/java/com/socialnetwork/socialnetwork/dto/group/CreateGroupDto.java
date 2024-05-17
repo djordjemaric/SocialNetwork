74b2aa3b140935cfcf7775df55b96c1c3b41292a
fnovakovic <143801644+fnovakovic@users.noreply.github.com>
2024-05-17 11:40:21 +0200

    Added changes to CreateGroupDto

--- src/main/java/com/socialnetwork/socialnetwork/dto/group/CreateGroupDto.java
+++ src/main/java/com/socialnetwork/socialnetwork/dto/group/CreateGroupDto.java
@@ -2,11 +2,12 @@ package com.socialnetwork.socialnetwork.dto.group;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateGroupDto(
         @NotBlank
         String name,
-        @NotBlank
+        @NotNull
         boolean isPublic) {
 
 
