a895513522a257eb5e9e3602f1e1a1dfe59ad233
Zeljko103 <zeljko.tanjevic@gmail.com>
2024-05-17 16:51:22 +0200

    security finished and tested, should be working now

--- src/main/java/com/socialnetwork/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/socialnetwork/socialnetwork/SocialNetworkApplication.java
@@ -1,16 +1,14 @@
 package com.socialnetwork.socialnetwork;
 
-import com.socialnetwork.socialnetwork.configuration.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class SocialNetworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/socialnetwork/socialnetwork/configuration/SecurityConfiguration.java
+++ src/main/java/com/socialnetwork/socialnetwork/configuration/SecurityConfiguration.java
@@ -1,72 +1,34 @@
 package com.socialnetwork.socialnetwork.configuration;
 
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    private final RsaKeyProperties keys;
-
-    public SecurityConfiguration(RsaKeyProperties keys) {
-        this.keys = keys;
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager user(){
-        return new InMemoryUserDetailsManager(
-                User.withUsername("test")
-                        .password("{noop}test123")
-                        .authorities("read")
-                        .build()
-        );
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             return http
-                    .csrf(csrf -> csrf.disable())
-                    .authorizeRequests(
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(
                             auth -> auth.anyRequest().authenticated()
                     )
-                    .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                     .sessionManagement(
                             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     )
-                    .httpBasic(Customizer.withDefaults())
+                    .httpBasic(withDefaults
+                            ())
                     .build();
     }
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(this.keys.pub_key()).build();
-    }
-
-    @Bean
-	JwtEncoder jwtEncoder() {
-		JWK jwk = new RSAKey.Builder(keys.pub_key()).privateKey(keys.priv_key()).build();
-		JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-		return new NimbusJwtEncoder(jwks);
-	}
 
 }
