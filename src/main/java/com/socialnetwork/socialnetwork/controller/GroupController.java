baedb67f050c4a414ececcda6b2dc6342a5663d7
fnovakovic <143801644+fnovakovic@users.noreply.github.com>
2024-05-20 16:09:52 +0200

    Added feature for creating group

---
+++ src/main/java/com/socialnetwork/socialnetwork/controller/GroupController.java
@@ -0,0 +1,29 @@
+package com.socialnetwork.socialnetwork.controller;
+
+import com.socialnetwork.socialnetwork.dto.group.CreateGroupDto;
+import com.socialnetwork.socialnetwork.entity.Group;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import com.socialnetwork.socialnetwork.service.GroupService;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/groups")
+public class GroupController {
+
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping( "/create")
+    public Group createGroup(@RequestBody CreateGroupDto createGroupDto) {
+        return groupService.createGroup(createGroupDto);
+    }
+
+
+}
---
+++ src/main/java/com/socialnetwork/socialnetwork/service/GroupService.java
@@ -0,0 +1,56 @@
+package com.socialnetwork.socialnetwork.service;
+
+import com.socialnetwork.socialnetwork.dto.group.CreateGroupDto;
+import com.socialnetwork.socialnetwork.entity.Group;
+import com.socialnetwork.socialnetwork.entity.User;
+import com.socialnetwork.socialnetwork.mapper.GroupMapper;
+import com.socialnetwork.socialnetwork.repository.GroupMemberRepository;
+import com.socialnetwork.socialnetwork.repository.UserRepository;
+import org.hibernate.query.sqm.produce.function.FunctionArgumentException;
+import org.springframework.stereotype.Service;
+import com.socialnetwork.socialnetwork.repository.GroupRepository;
+
+@Service
+public class GroupService {
+    private final GroupRepository groupRepository;
+    private final GroupMemberRepository groupMemberRepository;
+    private final GroupMapper groupMapper;
+
+    private final UserRepository userRepository;
+
+    private final JwtService jwtService;
+    public GroupService(GroupRepository groupRepository, GroupMapper groupMapper, GroupMemberRepository groupMemberRepository,UserRepository userRepository,JwtService jwtService) {
+        this.groupRepository = groupRepository;
+        this.groupMapper = groupMapper;
+        this.groupMemberRepository = groupMemberRepository;
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+
+    }
+
+    public Group createGroup(CreateGroupDto group) {
+        User currentUser = jwtService.getUser();
+
+        //provera da li user sa tim emailom postoji
+        if(!userRepository.existsByEmail(currentUser.getEmail())){
+            System.out.println(currentUser.getEmail());
+            throw new FunctionArgumentException("User with that email does not exists!!");
+        }
+
+        //provera da li postoji grupa sa tim imenom
+        if(groupRepository.existsByName(group.name())){
+            throw new FunctionArgumentException("Group with that name already exists");
+
+        }
+
+        //kreiranje grupe
+        Group createdGroup = groupRepository.save(groupMapper.createDtoToEntity(currentUser, group));
+
+        //dodavanje admina kao membera u tu grupu
+        groupMemberRepository.save(groupMapper.createGroupMemberEntity(currentUser, createdGroup));
+
+        return createdGroup;
+    }
+
+
+}
