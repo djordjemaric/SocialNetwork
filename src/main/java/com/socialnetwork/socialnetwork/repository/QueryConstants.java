be7b96ffe10a473db0c12f16ac64e827fdb7e588
RisticViktor <vica.ristic@gmail.com>
2024-05-20 09:48:37 +0200

    fixed an error in query for checking if two users are friends

--- src/main/java/com/socialnetwork/socialnetwork/repository/QueryConstants.java
+++ src/main/java/com/socialnetwork/socialnetwork/repository/QueryConstants.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class QueryConstants {
 
     public static final String ALL_REQUESTS_FROM_USER = "SELECT fr FROM FriendRequest fr WHERE fr.from.id = :userId OR fr.to.id = :userId";
-    public static final String ARE_TWO_USERS_FRIENDS = "SELECT fr FROM Friends fr WHERE (fr.friendTo.id = :idUser1 OR fr.friend.id = :idUser2) OR (fr.friendTo.id = :idUser2 OR fr.friend.id = :idUser1)";
+    public static final String ARE_TWO_USERS_FRIENDS = "SELECT fr FROM Friends fr WHERE (fr.friendTo.id = :idUser1 AND fr.friend.id = :idUser2) OR (fr.friendTo.id = :idUser2 AND fr.friend.id = :idUser1)";
 
 }
