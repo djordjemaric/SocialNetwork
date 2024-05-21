72fcbb1f490915f13bbae11bea747e2a3add8602
djordjemaric <djordjegmaric@gmail.com>
2024-05-21 16:40:19 +0200

    remove unused imports, fix return type of upload method

--- src/main/java/com/socialnetwork/socialnetwork/service/S3Service.java
+++ src/main/java/com/socialnetwork/socialnetwork/service/S3Service.java
@@ -1,45 +1,24 @@
 package com.socialnetwork.socialnetwork.service;
 
 import io.awspring.cloud.s3.S3Template;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.internal.sync.FileContentStreamProvider;
-import software.amazon.awssdk.http.*;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.Map;
 
 @Service
 public class S3Service {
     private final S3Template s3Template;
-//    private final S3Presigner s3Presigner;
 
     private String bucketName = "social-network-storage";
 
     public S3Service(S3Template s3Template) {
         this.s3Template = s3Template;
-
     }
 
     public String createPresignedDownloadUrl(String key) {
@@ -50,10 +29,11 @@ public class S3Service {
         return s3Template.createSignedPutURL(bucketName, key, Duration.ofMinutes(10)).toExternalForm();
     }
 
-    public void uploadToBucket(File file) {
+    public String uploadToBucket(File file) {
         String key = file.getName();
         String url = createPresignedUrl(key);
         useHttpClientToPut(url,file);
+        return key;
     }
 
     private void useHttpClientToPut(String presignedUrlString, File fileToPut) {
