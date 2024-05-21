430c4285f9f84fe54b0f06205908e02fcebc155e
RisticViktor <vica.ristic@gmail.com>
2024-05-21 12:07:22 +0200

    removed DeletedFriendDTO

--- src/main/java/com/socialnetwork/socialnetwork/dto/friends/DeletedFriendDTO.java
+++
@@ -1,8 +0,0 @@
-package com.socialnetwork.socialnetwork.dto.friends;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DeletedFriendDTO(
-        @NotBlank String deletedFriend
-) {
-}
