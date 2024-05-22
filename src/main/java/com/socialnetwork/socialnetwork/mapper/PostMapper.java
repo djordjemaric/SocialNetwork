1a4fe1989c38adfced2a3aa1549425f414190190
Ivana Lukovic <ivanalukovic01@gmail.com>
2024-05-22 10:55:42 +0200

    updated delete-post branch

--- src/main/java/com/socialnetwork/socialnetwork/mapper/PostMapper.java
+++ src/main/java/com/socialnetwork/socialnetwork/mapper/PostMapper.java
@@ -1,7 +1,6 @@
 package com.socialnetwork.socialnetwork.mapper;
 
 import com.socialnetwork.socialnetwork.dto.post.CreatePostDTO;
-import com.socialnetwork.socialnetwork.dto.post.PostDTO;
 import com.socialnetwork.socialnetwork.entity.Group;
 import com.socialnetwork.socialnetwork.entity.Post;
 import com.socialnetwork.socialnetwork.entity.User;
@@ -38,11 +37,5 @@ public class PostMapper {
 
         return post;
     }
-
-    public PostDTO postToPostDTO(Post post) {
-        return new PostDTO(
-                post.getId(), post.getText(), post.getImgUrl(), post.getOwner().getEmail(), post.getGroup().getName(), post.getComments());
-    }
-
 
 }
