dd08f7c6988b493b3edeec1c3bfa24279c68b54c
djordjemaric <djordjegmaric@gmail.com>
2024-05-22 09:43:58 +0200

    use upload method of s3template

--- src/main/java/com/socialnetwork/socialnetwork/service/S3Service.java
+++ src/main/java/com/socialnetwork/socialnetwork/service/S3Service.java
@@ -1,9 +1,11 @@
 package com.socialnetwork.socialnetwork.service;
 
 import io.awspring.cloud.s3.S3Template;
+import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -25,29 +27,20 @@ public class S3Service {
         return s3Template.createSignedGetURL(bucketName, key, Duration.ofMinutes(10)).toExternalForm();
     }
 
-    private String createPresignedUrl(String key) {
-        return s3Template.createSignedPutURL(bucketName, key, Duration.ofMinutes(10)).toExternalForm();
-    }
-
-    public String uploadToBucket(File file) {
-        String key = file.getName();
-        String url = createPresignedUrl(key);
-        useHttpClientToPut(url,file);
+    public String uploadToBucket(MultipartFile file) {
+        String key = file.getOriginalFilename();
+        if (key == null || key.isEmpty()) {
+            throw new IllegalArgumentException("File name cannot be null or empty");
+        }
+        try {
+            s3Template.upload(bucketName, key, file.getInputStream());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
         return key;
     }
 
-    private void useHttpClientToPut(String presignedUrlString, File fileToPut) {
-        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
-        HttpClient httpClient = HttpClient.newHttpClient();
-        try {
-            httpClient.send(requestBuilder
-                            .uri(new URL(presignedUrlString).toURI())
-                            .PUT(HttpRequest.BodyPublishers.ofFile(Path.of(fileToPut.toURI())))
-                            .build(),
-                    HttpResponse.BodyHandlers.discarding());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 }
