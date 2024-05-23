e91ffe5518fa1056a4c89d08315746b80e7269be
Iva Susa <ivasusa04@gmail.com>
2024-05-23 13:02:28 +0200

    wip_ai_text

---
+++ src/main/java/com/socialnetwork/socialnetwork/controller/PromptController.java
@@ -0,0 +1,22 @@
+package com.socialnetwork.socialnetwork.controller;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("prompt")
+@RestController
+public class PromptController {
+    private final ChatClient chatClient;
+
+    public PromptController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+    @GetMapping()
+    public String generatePrompt(){
+        return chatClient.call(new Prompt("tell me a funny joke")).getResult().getOutput().getContent();
+    }
+
+}
