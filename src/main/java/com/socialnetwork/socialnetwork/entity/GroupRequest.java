0886f8317ce25a1c3a3c70864c9ec1b71de99f75
fnovakovic <143801644+fnovakovic@users.noreply.github.com>
2024-05-23 13:28:21 +0200

    Added changes

--- src/main/java/com/socialnetwork/socialnetwork/entity/GroupRequest.java
+++ src/main/java/com/socialnetwork/socialnetwork/entity/GroupRequest.java
@@ -1,6 +1,5 @@
 package com.socialnetwork.socialnetwork.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
