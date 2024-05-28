86e3e1cb3d59d7d99dc0466d9df77580b2bbbba3
Ivana Lukovic <ivanalukovic01@gmail.com>
2024-05-28 14:18:52 +0200

    wip

--- src/test/java/com/socialnetwork/socialnetwork/controller/PostControllerTests.java
+++ src/test/java/com/socialnetwork/socialnetwork/controller/PostControllerTests.java
@@ -1,9 +1,9 @@
 package com.socialnetwork.socialnetwork.controller;
 
 import com.socialnetwork.socialnetwork.IntegrationTestConfiguration;
-import com.socialnetwork.socialnetwork.dto.post.PostDTO;
 import com.socialnetwork.socialnetwork.entity.Post;
 import com.socialnetwork.socialnetwork.entity.User;
+import com.socialnetwork.socialnetwork.exceptions.ResourceNotFoundException;
 import com.socialnetwork.socialnetwork.repository.PostRepository;
 import com.socialnetwork.socialnetwork.repository.UserRepository;
 import org.junit.jupiter.api.*;
@@ -12,19 +12,23 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PostControllerTests extends IntegrationTestConfiguration {
 
-    private String postApiURL="api/posts";
+    private final String postApiURL="/api/posts";
+
 
     @Autowired
     private PostRepository postRepository;
     @Autowired
     private UserRepository userRepository;
 
-    //napravi after all
+    @AfterEach
+    void emptyDatabase(){
+        postRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
     @Test
-    public void testGetById_privatePost_notFriends_throwsAccessDeniedException() throws Exception {
-        // Setup test data
-        User currentUser = jwtService.getUser();
+    public void does_getById_return_postDTO() throws ResourceNotFoundException {
+        User currentUser=jwtService.getUser();
         User testOwner = new User();
         testOwner.setUserSub("f3841812-e0f1-7025-b7bc-ce67d7fb933e");
         testOwner.setEmail("xanitev711@mcatag.com");
@@ -33,62 +37,28 @@ public class PostControllerTests extends IntegrationTestConfiguration {
         post.setOwner(testOwner);
         post.setPublic(false);
         post=postRepository.save(post);
-
-        PostDTO postDTO=restTemplate.getForObject(postApiURL+"/"+post.getId(), PostDTO.class);
-
-        assertThat()
-    }
-
-    @Test
-    public void testGetById_GroupPost_NotMember_ThrowsAccessDeniedException() throws Exception {
-        // Setup test data
-        User user = createUser();
-        Group group = createPrivateGroup();
-        User owner = createUser("owner");
-        Post post = createGroupPost(owner, group);
-        saveEntities(user, owner, group, post);
-       }
-
-    // Helper methods for creating test data
-    private Post createPublicPost(User owner) {
-        Post post = new Post();
-        post.setOwner(owner);
-        post.setPublic(true);
-        return postRepository.save(post);
-    }
-
-    private Post createPrivatePost(User owner) {
-        Post post = new Post();
-        post.setOwner(owner);
-        post.setPublic(false);
-        return postRepository.save(post);
-    }
-
-    private Group createPrivateGroup() {
-        Group group = new Group();
-        group.setName("Private Group");
-        group.setPublic(false);
-        return groupRepository.save(group);
-    }
-
-    private Post createGroupPost(User owner, Group group) {
-        Post post = new Post();
-        post.setOwner(owner);
-        post.setGroup(group);
-        return postRepository.save(post);
-    }
-
-    private void saveEntities(Object... entities) {
-        for (Object entity : entities) {
-            if (entity instanceof User) {
-                userRepository.save((User) entity);
-            } else if (entity instanceof Post) {
-                postRepository.save((Post) entity);
-            } else if (entity instanceof Group) {
-                groupRepository.save((Group) entity);
-            }
-        }
     }
 
 
+
+//    @Test
+//    public void testGetById_privatePost_notFriends_throwsAccessDeniedException() throws Exception {
+//        // Setup test data
+////        User currentUser=jwtService.getUser();
+//        User testOwner = new User();
+//        testOwner.setUserSub("f3841812-e0f1-7025-b7bc-ce67d7fb933e");
+//        testOwner.setEmail("xanitev711@mcatag.com");
+//        testOwner = userRepository.save(testOwner);
+//        Post post = new Post();
+//        post.setOwner(testOwner);
+//        post.setPublic(false);
+//        post=postRepository.save(post);
+//
+//        when(jwtService.getUser()).thenReturn(testOwner);
+//
+//        Post finalPost = post;
+//        assertThrows(AccessDeniedException.class, () -> {
+//            postService.getById(finalPost.getId());
+//        });
+//    }
 }
