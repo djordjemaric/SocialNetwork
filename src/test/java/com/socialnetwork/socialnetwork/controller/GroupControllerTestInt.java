1e4b6634cae21e57dbadb3497288351a513f6e6e
fnovakovic <143801644+fnovakovic@users.noreply.github.com>
2024-05-28 12:07:59 +0200

    Added changes

--- src/test/java/com/socialnetwork/socialnetwork/controller/GroupControllerTestInt.java
+++ src/test/java/com/socialnetwork/socialnetwork/controller/GroupControllerTestInt.java
@@ -2,9 +2,12 @@ package com.socialnetwork.socialnetwork.controller;
 
 import com.socialnetwork.socialnetwork.IntegrationTestConfiguration;
 import com.socialnetwork.socialnetwork.dto.friendRequest.FriendRequestDTO;
+import com.socialnetwork.socialnetwork.dto.group.CreateGroupDTO;
 import com.socialnetwork.socialnetwork.dto.group.GroupDTO;
+import com.socialnetwork.socialnetwork.dto.group.GroupRequestDTO;
 import com.socialnetwork.socialnetwork.entity.FriendRequest;
 import com.socialnetwork.socialnetwork.entity.Group;
+import com.socialnetwork.socialnetwork.entity.GroupRequest;
 import com.socialnetwork.socialnetwork.entity.User;
 import com.socialnetwork.socialnetwork.exceptions.ResourceNotFoundException;
 import com.socialnetwork.socialnetwork.repository.FriendRequestRepository;
@@ -71,5 +74,67 @@ class GroupControllerTestInt extends IntegrationTestConfiguration {
         GroupDTO[] grResponseArray = restTemplate.getForObject(urlWithParams, GroupDTO[].class);
 
         assertThat(grResponseArray.length).isEqualTo(1);
+    }
+
+
+    @Test
+    void getAllRequestForGroup() throws ResourceNotFoundException {
+        User testUser1 = new User();
+        testUser1.setUserSub("f3841812-e0f1-7025-b7bc-ce67d7fb933e");
+        testUser1.setEmail("xanitev711@mcatag.com");
+        testUser1 = userRepository.save(testUser1);
+
+
+
+        Group testGroup = new Group();
+        testGroup.setName("Test");
+        testGroup.setPublic(false);
+        testGroup.setPosts(null);
+        testGroup.setAdmin(testUser1);
+        groupRepository.save(testGroup);
+
+        GroupRequest testGroupRequest =  new GroupRequest();
+        testGroupRequest.setUser(testUser1);
+        testGroupRequest.setGroup(testGroup);
+
+        String urlWithParams = UriComponentsBuilder.fromUriString(groupsApiURL)
+                .path("/{id}/requests")
+                .buildAndExpand(testGroup.getId())
+                .toUriString();
+
+        GroupRequestDTO[] grRequestsResponseArray = restTemplate.getForObject(urlWithParams, GroupRequestDTO[].class);
+
+        assertThat(grRequestsResponseArray.length).isEqualTo(1);
+    }
+
+    @Test
+    void createGroup() throws ResourceNotFoundException {
+        User testUser1 = new User();
+        testUser1.setUserSub("f3841812-e0f1-7025-b7bc-ce67d7fb933e");
+        testUser1.setEmail("xanitev711@mcatag.com");
+        testUser1 = userRepository.save(testUser1);
+
+        CreateGroupDTO createGroupDTO = new CreateGroupDTO("Test",false);
+
+
+        Group testGroup = new Group();
+        testGroup.setName("Test");
+        testGroup.setPublic(false);
+        testGroup.setPosts(null);
+        testGroup.setAdmin(testUser1);
+        groupRepository.save(testGroup);
+
+        GroupRequest testGroupRequest =  new GroupRequest();
+        testGroupRequest.setUser(testUser1);
+        testGroupRequest.setGroup(testGroup);
+
+        String urlWithParams = UriComponentsBuilder.fromUriString(groupsApiURL)
+                .path("/")
+                .buildAndExpand(testGroup.getId())
+                .toUriString();
+
+        GroupRequestDTO[] grRequestsResponseArray = restTemplate.postForObject(urlWithParams,createGroupDTO,GroupRequestDTO[].class);
+
+        assertThat(grRequestsResponseArray.length).isEqualTo(1);
     }
 }
