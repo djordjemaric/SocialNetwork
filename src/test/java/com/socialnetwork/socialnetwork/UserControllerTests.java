748dccb6f9a07f2ae126e87f6eef5cc7084779a1
djordjemaric <djordjegmaric@gmail.com>
2024-05-29 10:57:27 +0200

    add usercontroller tests

--- src/test/java/com/socialnetwork/socialnetwork/UserControllerTests.java
+++ src/test/java/com/socialnetwork/socialnetwork/UserControllerTests.java
@@ -3,10 +3,13 @@ package com.socialnetwork.socialnetwork;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.socialnetwork.socialnetwork.controller.UserController;
-import com.socialnetwork.socialnetwork.dto.UserRequest;
+import com.socialnetwork.socialnetwork.dto.user.LoginResponse;
+import com.socialnetwork.socialnetwork.dto.user.PreviewUserDTO;
+import com.socialnetwork.socialnetwork.dto.user.UserRequest;
 import com.socialnetwork.socialnetwork.entity.User;
 import com.socialnetwork.socialnetwork.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -35,33 +38,58 @@ public class UserControllerTests {
     @InjectMocks
     private UserController userController;
     private UserRequest userRequest;
-
+    private User user;
+    private PreviewUserDTO expectedUserDTO;
+    private LoginResponse expectedLoginResponse;
 //    private LoginResponse loginResponse;
 
     @BeforeEach
     public void setUp() {
         this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
         userRequest = new UserRequest("user@user.com", "test");
+        user = new User(1, "user@user.com", "sample-user-sub");
+        expectedUserDTO = new PreviewUserDTO(user.getId(), user.getEmail());
+        expectedLoginResponse = new LoginResponse("access", "refresh", 1000);
     }
 
+    @Test
     void createUser_success() throws Exception {
-//        User user = new User(1, "user@user.com", "sample-user-sub");
-//        when(userService.createUser(userRequest.email(), userRequest.password())).thenReturn(user);
-//
-//        String content = objectWriter.writeValueAsString(userRequest);
-//
-//        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/users")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON)
-//                .content(content);
-//
-//        mockMvc.perform(mockRequest)
-//                .andExpect(status().isCreated())
-//                .andExpect(result -> {
-//                    String responseBody = result.getResponse().getContentAsString();
-//                    UserRequest actualUserRequest = objectMapper.readValue(responseBody, UserRequest.class);
-//                    assertEquals(userRequest,)
-//                });
 
+        when(userService.createUser(userRequest.email(), userRequest.password())).thenReturn(expectedUserDTO);
+
+        String content = objectWriter.writeValueAsString(userRequest);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/users/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content);
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isCreated())
+                .andExpect(result -> {
+                    String responseBody = result.getResponse().getContentAsString();
+                    PreviewUserDTO actualUserDTO = objectMapper.readValue(responseBody, PreviewUserDTO.class);
+                    assertEquals(actualUserDTO, expectedUserDTO);
+                });
+    }
+
+    @Test
+    void loginUser_success() throws Exception {
+        when(userService.loginUser(userRequest.email(), userRequest.password())).thenReturn(expectedLoginResponse);
+
+        String content = objectWriter.writeValueAsString(userRequest);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content);
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(result -> {
+                    String responseBody = result.getResponse().getContentAsString();
+                    LoginResponse actualLoginResponse = objectMapper.readValue(responseBody, LoginResponse.class);
+                    assertEquals(actualLoginResponse, expectedLoginResponse);
+                });
     }
 }
