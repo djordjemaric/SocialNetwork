8d5a16efe017b7214c739777368c2f99b3376fc8
fnovakovic <143801644+fnovakovic@users.noreply.github.com>
2024-05-31 09:27:09 +0200

    Added changes

--- src/test/java/com/socialnetwork/socialnetwork/GroupControllerTests.java
+++ src/test/java/com/socialnetwork/socialnetwork/GroupControllerTests.java
@@ -55,7 +55,6 @@ class GroupControllerTests {
     private PreviewUserDTO previewUserDTO;
     private GroupDTO expectedGroupDTO;
     private List<GroupDTO> expectedGroupDTOS;
-    private List<PostDTO> expectedPostDTOS;
 
     @BeforeEach
     public void setUp() {
@@ -66,13 +65,11 @@ class GroupControllerTests {
                 new GroupDTO("Group1", "admin1@admin.com", true, 1),
                 new GroupDTO("Group2", "admin2@admin.com", true, 2)
         );
-        // Create mock Users
         User user1 = new User(1, "user1@example.com", "user1Sub");
         User user2 = new User(2, "user2@example.com", "user2Sub");
 
-        // Create mock Group
         Group group = new Group(1, "Group1", user1, true, null);
-        // Create mock Comments
+
         Comment comment1 = new Comment(1, "Comment text 1", LocalDateTime.now(), user1, Collections.emptyList(), null);
         Comment comment2 = new Comment(2, "Comment text 2", LocalDateTime.now(), user2, Collections.emptyList(), null);
 
@@ -80,19 +77,11 @@ class GroupControllerTests {
         comments.add(comment1);
         comments.add(comment2);
 
-        // Create mock Posts
         Post post1 = new Post(1, true, "Post text 1", "imgUrl1", user1, group, comments);
         Post post2 = new Post(2, true, "Post text 2", "imgUrl2", user2, group, comments);
 
-
-        // Set posts for group
         group.setPosts(Arrays.asList(post1, post2));
 
-        // Create PostDTO objects
-        expectedPostDTOS = Arrays.asList(
-                new PostDTO(1, "Post text 1", "imgUrl1", "user1@example.com", "Group1", List.of(comment1)),
-                new PostDTO(2, "Post text 2", "imgUrl2", "user2@example.com", "Group1", List.of(comment2))
-        );
         previewUserDTO = new PreviewUserDTO(1, "user@example.com");
         resolvedGroupRequestDTO = new ResolvedGroupRequestDTO(1, previewUserDTO, expectedGroupDTO, ResolvedGroupRequestStatus.REQUEST_TO_JOIN_GROUP_ACCEPTED);
 
@@ -100,20 +89,15 @@ class GroupControllerTests {
 
     @Test
     void createGroup_success() throws Exception {
-
         when(groupService.createGroup(createGroupDTO)).thenReturn(expectedGroupDTO);
 
         String content = objectWriter.writeValueAsString(createGroupDTO);
 
-
-        //pravimo request
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/groups")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(content);
 
-
-        //izvrsavanje requesta
         mockMvc.perform(mockRequest)
                 .andExpect(status().isCreated())
                 .andExpect(result -> {
@@ -155,21 +139,16 @@ class GroupControllerTests {
 
     }
 
-
-
     @Test
     void findGroupsByName_success() throws Exception {
         String groupName = "Group";
 
         when(groupService.findByName(groupName)).thenReturn(expectedGroupDTOS);
 
-        //pravimo request
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/api/groups")
                 .param("name", groupName)
                 .accept(MediaType.APPLICATION_JSON);
 
-
-        //izvrsavanje requesta
         mockMvc.perform(mockRequest)
                 .andExpect(status().isOk())
                 .andExpect(result -> {
@@ -177,25 +156,6 @@ class GroupControllerTests {
                     List<GroupDTO> actualGroupDTOS = objectMapper.readValue(responseBody, new TypeReference<>() {
                     });
                     assertEquals(expectedGroupDTOS, actualGroupDTOS);
-                });
-    }
-
-    @Test
-    void findGroupsByName_throwsException() throws Exception {
-        String groupName = "Test Group";
-        // Mock the method to throw the exception
-        doThrow(new FunctionArgumentException("Group search failed"))
-                .when(groupService).findByName(anyString());
-
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/api/groups")
-                .param("name", groupName)
-                .accept(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(mockRequest)
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> {
-                    String responseBody = result.getResponse().getContentAsString();
-                    assertEquals("Group search failed", objectMapper.readTree(responseBody).get("message").asText());
                 });
     }
 
@@ -213,38 +173,14 @@ class GroupControllerTests {
         verify(groupService).deleteGroup(idGroup);
     }
 
-
-    @Test
-    void deleteGroup_throwsException() throws Exception {//ne radi
-        Integer idGroup = 1;
-
-        // Throw an exception when deleteGroup is called with idGroup
-        doThrow(new FunctionArgumentException("Invalid group ID")).when(groupService).deleteGroup(idGroup);
-
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
-                .delete("/api/groups/{idGroup}", idGroup)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(mockRequest)
-                .andExpect(status().isBadRequest()) // Expecting a 400 Bad Request status
-                .andExpect(jsonPath("$.message").value("Invalid group ID")); // Verify the error message
-
-        verify(groupService).deleteGroup(idGroup);
-    }
-
-
-
     @Test
     void createRequestToJoinGroup_success() throws Exception {
         Integer id = 1;
         when(groupService.createRequestToJoinGroup(id)).thenReturn(resolvedGroupRequestDTO);
 
-        //pravimo request
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/groups/{id}/join", id)
                 .accept(MediaType.APPLICATION_JSON);
 
-
-        //izvrsavanje requesta
         mockMvc.perform(mockRequest)
                 .andExpect(status().isCreated())
                 .andExpect(result -> {
@@ -253,28 +189,5 @@ class GroupControllerTests {
                     assertEquals(resolvedGroupRequestDTO, actualResolvedGroupRequestDTO);
                 });
     }
-
-    @Test
-    void getPostsByGroupId_success() throws Exception {
-        Integer id = 1;
-        when(groupService.getAllPostsByGroupId(id)).thenReturn(expectedPostDTOS);
-
-        //pravimo request
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/api/groups/{id}/posts", id)
-                .accept(MediaType.APPLICATION_JSON);
-
-
-        //izvrsavanje requesta
-        mockMvc.perform(mockRequest)
-                .andExpect(status().isOk())
-                .andExpect(result -> {
-                    String responseBody = result.getResponse().getContentAsString();
-                    List<PostDTO> actualPostDTOS = objectMapper.readValue(responseBody, new TypeReference<>() {
-                    });
-                    assertEquals(expectedPostDTOS, actualPostDTOS);
-                });
-    }
-
-
 
 }
