97a33562fabf4c3580693d09be1e247a13334575
Zeljko103 <zeljko.tanjevic@gmail.com>
2024-06-04 10:59:55 +0200

    added dummy route for testing CI/CD

--- src/main/java/com/socialnetwork/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/socialnetwork/socialnetwork/SocialNetworkApplication.java
@@ -2,11 +2,22 @@ package com.socialnetwork.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SocialNetworkApplication {
 
+	@ResponseStatus(HttpStatus.OK)
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from SocialNetwork";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
 	}
 }
+
