d69b91cb7c79844020797cf3f38719cedc748005
djordjemaric <djordjegmaric@gmail.com>
2024-06-04 13:04:54 +0200

    update hello route to show pipeline

--- src/main/java/com/socialnetwork/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/socialnetwork/socialnetwork/SocialNetworkApplication.java
@@ -14,7 +14,7 @@ public class SocialNetworkApplication {
 	@ResponseStatus(HttpStatus.OK)
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from SocialNetwork";
+        return "Hello from demo! Pipeline is working good.";
     }
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
